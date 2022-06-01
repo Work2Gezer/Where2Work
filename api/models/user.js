2d446389c44a6c1008028aa2b56e849cb551476d
nope <nope@vmi843734.contaboserver.net>
2022-06-01 23:38:14 +0200

    Add somes api routes for users

--- api/models/user.js
+++ api/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 const bcrypt = require('bcrypt')
 
 const userSchema = mongoose.Schema({
+    id: { type: String, required: true, index: { unique: true } },
     username: { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true },
     email: { type: String, required: true, index: { unique: true } },
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,13 +1,43 @@
 const express = require('express');
+const User = require('../models/user');
 const router = express.Router();
+const short = require('short-uuid');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// Get all users
+router.get("/", async (req, res) => {
+	const users = await User.find()
+	res.send(posts)
+})
 
+//Get one user by id
 router.get('/:id', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// Create a new user
+router.post("/", async (req, res) => {
+	const user = new User({
+		id : short.generate(),
+    username : req.body.username,
+    password : req.body.password,
+    email : req.body.email,
+    permissions: req.body.permissions ?? "user",
+	})
+	await user.save()
+	res.send(post)
+})
+
+//Create a new admin path (url: /api/users/admin)
+router.post("/admin", async (req, res) => {
+	const user = new User({
+		id : short.generate(),
+    username : req.body.username,
+    password : req.body.password,
+    email : req.body.email,
+    permissions: req.body.permissions ?? "admin",
+	})
+	await user.save()
+	res.send(post)
+})
 
 module.exports = router;
