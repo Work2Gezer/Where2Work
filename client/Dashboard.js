adbb49cf18e0cd9fd3a80174aa900e03ec200364
damien madeira <damien.dylan77@gmail.com>
2022-06-20 11:12:59 +0200

    add routes

---
+++ client/Dashboard.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableHighlight, ScrollView, Button } from 'react-native';
+
+const Dashboard = ({ navigation: { navigate } }) => {
+
+
+  return (
+    <View style={styles.container}>
+      <Text>TEST DASHBOARDDD</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default Dashboard;
